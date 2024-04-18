22fe878d32a99ad9a4b48c30eb229d61eb35c887
Lingaraj <lingaraj.kunda@covalenseglobal.com>
2024-04-19 00:34:14 +0530

    custom hook and internet connection check

--- src/routing-n-error-handling/About.js
+++ src/routing-n-error-handling/About.js
@@ -1,7 +1,22 @@
 import React from "react";
+import useOnlineStatus from "../utils/useOnlineStatus";
+import Spinner from "react-bootstrap/Spinner";
 
 const About = () => {
-  return <div>About</div>;
+  const onliestatus = useOnlineStatus();
+  return (
+    <>
+      <div>About</div>
+      <p>
+        Onlie Status:{" "}
+        {onliestatus ? (
+          <Spinner animation="grow" variant="success" />
+        ) : (
+          <Spinner animation="grow" variant="danger" />
+        )}
+      </p>
+    </>
+  );
 };
 
 export default About;
--- src/utils/useOnlineStatus.js
+++ src/utils/useOnlineStatus.js
@@ -1,1 +1,15 @@
-//Hiiii
+//Custom hook creation and internet connect check.
+import { useState, useEffect } from "react";
+const useOnlineStatus = () => {
+  const [onleStatus, setOnlineStatus] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("offline", (event) => {
+      setOnlineStatus(false);
+    });
+  }, []);
+
+  return onleStatus;
+};
+
+export default useOnlineStatus;
